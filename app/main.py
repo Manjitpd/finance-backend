149d52c434fb2f3a78039964433c26b4860696d8
Manjitpd <kumarmanjit9473@gmail.com>
2026-04-02 15:30:49 +0530

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from app.api.routes import auth, users, finance, dashboard
 from app.db.base import Base
 from app.db.session import engine
+from fastapi.middleware.cors import CORSMiddleware 
+
+
 
 app = FastAPI(title="Finance Dashboard API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
